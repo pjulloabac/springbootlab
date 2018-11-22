9990050ae6d6abe2cc20e53ffceab68d826ef44f
geanque <33296257+geanque@users.noreply.github.com>
2018-11-22 15:23:57 -0600

    * Se incluye el entity

---
+++ auth/src/main/java/domain/User.java
@@ -0,0 +1,114 @@
+package domain;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity
+public class User implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -341097569668277188L;
+
+	@Id
+	private String id;
+	
+	@Column
+	private String pass;
+	
+	@Column
+	private String name;
+
+	
+	public User(String id, String pass, String name) {
+		super();
+		this.id = id;
+		this.pass = pass;
+		this.name = name;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getPass() {
+		return pass;
+	}
+
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", pass=" + pass + ", name=" + name + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((pass == null) ? 0 : pass.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (pass == null) {
+			if (other.pass != null)
+				return false;
+		} else if (!pass.equals(other.pass))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+
+
+}
