764fac72c2d2afb7425426a9afb33823bd757e4e
Guido Alonso Delgado Hernandez <guido.delgado@gmail.com>
2018-11-22 15:26:47 -0600

    * Se crea un cascaron de los datos y el repositorio para leer la base de datos

---
+++ auth/src/main/java/com/baccredomatic/auth/db/repository/UsersRepository.java
@@ -0,0 +1,11 @@
+package com.baccredomatic.auth.db.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.User;
+
+@Repository
+public interface UsersRepository extends CrudRepository<String, User> {
+
+}
---
+++ auth/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO USERS (USER_ID, USER_PASS, USER_NAME) VALUES ('gquesada', 'gquesada', 'Gerardo Quesada');
+INSERT INTO USERS (USER_ID, USER_PASS, USER_NAME) VALUES ('gdelgadoh', 'gdelgadoh', 'Guido Delgado');
+INSERT INTO USERS (USER_ID, USER_PASS, USER_NAME) VALUES ('rhernandez', 'rhernandez', 'Roberto Hernandez');
+INSERT INTO USERS (USER_ID, USER_PASS, USER_NAME) VALUES ('eblancol', 'eblancol', 'Elvia Blanco Lopez');
+INSERT INTO USERS (USER_ID, USER_PASS, USER_NAME) VALUES ('ksegura', 'ksegura', 'Karen Segura');
+INSERT INTO USERS (USER_ID, USER_PASS, USER_NAME) VALUES ('jgonzaleza', 'jgonzaleza', 'Juan Ramon Gonzalez');
+INSERT INTO USERS (USER_ID, USER_PASS, USER_NAME) VALUES ('fsalazarv', 'fsalazarv', 'Fabian Salazar');
