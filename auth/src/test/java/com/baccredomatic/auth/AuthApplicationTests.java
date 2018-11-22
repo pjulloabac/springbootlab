a614398331dac6e989b8cdcc7b250e0be64e0893
Guido Alonso Delgado Hernandez <guido.delgado@gmail.com>
2018-11-22 14:42:14 -0600

    Primera version del ejemplo de autenticacion

---
+++ auth/src/test/java/com/baccredomatic/auth/AuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baccredomatic.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
